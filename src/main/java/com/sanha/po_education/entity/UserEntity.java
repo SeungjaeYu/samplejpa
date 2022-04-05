689de56b94500d5b3355b821214da4fd28ff8125
YSJHAHA-PC\ysjhaha-PC <ysjhaha11@gmail.com>
2022-04-05 22:46:11 +0900

    jpa sample first ver

---
+++ src/main/java/com/sanha/po_education/entity/UserEntity.java
@@ -0,0 +1,51 @@
+package com.sanha.po_education.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "TB_USER")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity {
+
+    @Id // primary Key
+    private String email;
+
+
+    private String name;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+
+}
