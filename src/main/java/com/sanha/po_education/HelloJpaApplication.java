37918c71771d8d554836222a866b98d95a643f93
YSJHAHA-PC\ysjhaha-PC <ysjhaha11@gmail.com>
2022-04-13 14:44:16 +0900

    dd

--- src/main/java/com/sanha/po_education/HelloJpaApplication.java
+++ src/main/java/com/sanha/po_education/HelloJpaApplication.java
@@ -14,11 +14,14 @@ public class HelloJpaApplication {
 
 	public static void main(String[] args) {
 
+		String adad;
+
+
 		// EntityManagerFactory는 EntityManager를 생성하기 위한 팩토리 인터페이스로 persistence 단위별로 팩토리를 생성
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory(
 				"hello"); // persistence.xml 파일에 기입한 이름을 적어줘야 함
 
-		System.out.println("Check 1"); 
+		System.out.println("Check 1");
 
 		// EntityManager 객체를 생성
 		// EntityManager 는 Persistence Context와 Entity를 관리, 하나의 커넥션/세션으로 보면 됨
